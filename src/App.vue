fb6308827ca1df81698feafb28039a99c7b81c13
JS是最好的语言 <iiii11111ii11111ii@qq.com>
2019-07-17 15:46:40 +0800

    vue项目初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <router-link>确定</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>login</div>
+    <router-link>登录</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
